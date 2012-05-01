52288818d641b1c67368cd48d95ee89e37f4080e
Jerry Novy <ganovy@comcast.net>
2012-04-30 23:24:45 -0500

    Initial commit on Mnd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cafa81c2ce5e2043e72ac8024c7a34a2a5f954ebd8202d7746fb55f21b4ae45ea8faaffd1c861eb53b9ff74548e84f897c1593825f36befb349d8e1dae6967fe'
